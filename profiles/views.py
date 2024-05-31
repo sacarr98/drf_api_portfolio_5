80f37dd3bf966c761439ce38a232cfc200d5bdd1
Sophie Carr <sacarr98@gmail.com>
2024-05-31 11:26:23 +0000

    create profile list view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
--- project_api/settings.py
+++ project_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
 
     'profiles',
 ]

43df8eb342ace075b23f2951005b2a4f3230a8a9
Sophie Carr <sacarr98@gmail.com>
2024-06-05 15:19:41 +0000

    prepare code for deployment

--- project_api/settings.py
+++ project_api/settings.py
@@ -34,6 +34,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'project_api.serializers.CurrentUserSerializer'
@@ -47,11 +48,11 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '8000-sacarr98-drfapiportfoli-40vhjkqr2wp.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = ['localhost', '8000-sacarr98-drfapiportfoli-40vhjkqr2wp.ws-eu114.gitpod.io', 'https://fitness-api-0f45fd642eff.herokuapp.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-sacarr98-drfapiportfoli-40vhjkqr2wp.ws-eu114.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://8000-sacarr98-drfapiportfoli-40vhjkqr2wp.ws-eu114.gitpod.io', 'https://fitness-api-0f45fd642eff.herokuapp.com']
 
 
 # Application definition
@@ -85,6 +86,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -93,6 +95,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+     CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+     ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'project_api.urls'
 

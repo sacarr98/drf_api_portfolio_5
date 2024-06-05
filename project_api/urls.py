090133c46ef86fd839e831f11a08f299f257e210
Sophie Carr <sacarr98@gmail.com>
2024-06-05 15:05:06 +0000

    add views.py file to project_api and add pagination

--- project_api/urls.py
+++ project_api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
+from .views import root_route
 
 from exercise.urls import exercise_router
 
@@ -8,6 +9,7 @@ router = DefaultRouter()
 router.registry.extend(exercise_router.registry)
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
